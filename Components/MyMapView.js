6f6cc5baba8dc9ed0155309662beebdc460a7b9b
nahin <nahin@usbd.com.bd>
2022-12-14 16:56:54 +0600

    fift commit

--- Components/MyMapView.js
+++ Components/MyMapView.js
@@ -3,33 +3,50 @@ import { Text, View, StyleSheet, TextInput,
   Button , TouchableOpacity, ScrollView} 
 from 'react-native';
 import Constants from 'expo-constants';
-import MapView, {Marker} from "react-native-maps"
+import MapView, {Marker, Polyline} from "react-native-maps"
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import Axios from "axios"
 
+var polyline = require('@mapbox/polyline');
 
 export default function MyMapView() {
     
     const API_KEY = "NDQyOToxSU1QVFhSSkVZ";
     const [source, setSource] = useState("Burger Lab");
-    const [bangla, setBangla] = useState(false);
+	const [sCoord, setSCoord] = useState([]);
+
+	const [activeInput, setActiveInput] = useState(0)
+
+	const [destination, setDestination] = useState("BRAC University")
+	const [dCoord, setDCoord] = useState([]);
+
     const [places, setPlaces] = useState([]);
-  const [suggestions, setSuggestions] = useState([]);
+  	const [suggestions, setSuggestions] = useState([]);
+
+	const [waypoints, setWaypoints] = useState([])
+
 
 
   const find_place_request=()=>{
     // console.log("place name: ", placeName);
+	let x = (activeInput == 1) ? source : destination
     Axios.get(
-      "https://barikoi.xyz/v1/api/search/autocomplete/"+API_KEY+"/place?q="+source
+      "https://barikoi.xyz/v1/api/search/autocomplete/"+API_KEY+"/place?q="+x
     )
     .then( (res)=>{setPlaces(res.data["places"]);} )
     // .then( (res)=>{console.log(res.data["places"]);} )
     .catch( (err)=>{console.log(err);} )
   }
 
-  const onSearchHandler=(text)=>{
-    setSource(text)
+  const onSearchHandler=(number, text)=>{
+	if(number ==1){
+		setSource(text)
+	}else{
+		setDestination(text)
+	}
+
     let searchValue = text;
+	// console.log(searchValue);
     if(searchValue == ""){
       setSuggestions([]);
     }else{
@@ -47,34 +64,68 @@ export default function MyMapView() {
   }
 
   const onSuggestionPress=(item)=>{
-    console.log("Suggestion press");
-    setSource(item["address"]);
+    // console.log("Suggestion press");
+	if(activeInput == 1){
+		setSCoord([item])
+		setSource(item["address"]);
+	}else{
+		setDestination(item["address"]);
+		setDCoord([item])
+	}
     setPlaces([item])
   }
+
+  const onFindRoute=()=>{
+	console.log("Pressed find route");
+	console.log(sCoord);
+	console.log(dCoord);
+
+	let source = {"latitude": sCoord[0]["latitude"], "longitude":  sCoord[0]["longitude"]};
+  	let destination = {"latitude": dCoord[0]["latitude"], "longitude":  dCoord[0]["longitude"]};
+
+	console.log(source, destination);
+
+
+	const urls = "https://barikoi.xyz/v1/api/route/"+API_KEY+"/"+source["longitude"]+","+source["latitude"]+";"+destination["longitude"]+","+destination["latitude"]+""
+	Axios({
+		method: 'get',
+		url: urls,
+	})
+	.then((res)=>{
+		let polyString = res.data["routes"][0]["geometry"];
+		console.log(polyString);
+		polyString = polyline.decode(polyString);
+		let x = []
+		for(let a of polyString){
+			x.push({latitude: a[0], longitude: a[1]})
+		}
+		// console.log(x);
+
+		setPlaces([])
+		setWaypoints(x)
+	})
+	.catch((err)=>console.log(err.response.data))
+  }
+
+
+
     return(
         <View style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
 
             <Text style={{fontSize: 25}}>Search a place</Text>
+
+			<Text>Active {activeInput}, source text: {source}, destination text: {destination}</Text>
             
             {/* find place view */}
-            <View style={{flexDirection: "row", margin: 5}}>
+            <View style={{flexDirection: "row", margin: 5,}}>
             	<Text style={{ marginRight: 10}}>Source Name: </Text>
 				<TextInput 
 					style={styles.textInput}
 					placeholder="source place"
-					onChangeText={(text)=>{onSearchHandler(text)}}
-				/>
-
-            	<Text style={{ marginRight: 10}}>Bangla: </Text>
-            	<BouncyCheckbox onPress={(isChecked) => {setBangla(isChecked)}} />
-            </View>
-
-			<View style={{flexDirection: "row", margin: 5}}>
-            	<Text style={{ marginRight: 10}}>Destination Name: </Text>
-				<TextInput 
-					style={styles.textInput}
-					placeholder="Destination place"
-					onChangeText={(text)=>{onSearchHandler(text)}}
+					onChangeText={(text)=>{
+						setActiveInput(1)
+						onSearchHandler(1, text)
+					}}
 				/>
 
 				<TouchableOpacity
@@ -83,12 +134,32 @@ export default function MyMapView() {
 				>
 					<Text>Find Place</Text>
 				</TouchableOpacity>
+
+            </View>
+
+			<View style={{flexDirection: "row", margin: 5}}>
+            	<Text style={{ marginRight: 10}}>Destination Name: </Text>
+				<TextInput 
+					style={styles.textInput}
+					placeholder="Destination place"
+					onChangeText={(text)=>{
+						setActiveInput(2)
+						onSearchHandler(2, text)
+					}}
+				/>
+				<TouchableOpacity
+					onPress={()=>{onFindRoute()}}
+					style={styles.findPlace}
+				>
+					<Text>Find route</Text>
+				</TouchableOpacity>
+				
             </View>
                 
             
 
             {/* suggestion view */}
-            <ScrollView style={{width: "100%", height: 150, borderColor: "black", borderWidth: 1, borderRadius: 5}}>
+            <ScrollView style={styles.suggestionView}>
             {suggestions.map((item)=>{
                 return (
                     <TouchableOpacity
@@ -124,6 +195,25 @@ export default function MyMapView() {
                 
                 })}
 
+				{waypoints.map((items, index) => {
+					console.log(items);
+					return(
+						<Marker
+							key={index}
+							coordinate={items}
+							title={"waypoint "+index}
+							description={""}
+						/>
+					)
+					
+				})}
+
+				<Polyline
+					coordinates={waypoints}
+					strokeColor="#000"
+					strokeWidth={3}
+				/>
+
 
             </MapView>
 
@@ -137,9 +227,17 @@ const styles = StyleSheet.create({
     
     map: {
 		width: '100%',
-		height: '60%',
+		height: '62%',
     },
   
+	suggestionView:{
+		width: "100%", 
+		height: 120, 
+		borderColor: "black", 
+		borderWidth: 1, 
+		borderRadius: 5
+	},
+
     suggestionBtn: {
 		height: 20,
 		// width: 50,
@@ -157,7 +255,7 @@ const styles = StyleSheet.create({
 		borderWidth: 1,
 		width: "35%",
 		textAlign: "center",
-		marginRight: 10
+		marginRight: 5
 		
 	},
 
@@ -167,7 +265,6 @@ const styles = StyleSheet.create({
 		width: 100, height: 30,
 		justifyContent: "center",
 		borderRadius: 10,
-		margin: 5,
 	},
   
   
